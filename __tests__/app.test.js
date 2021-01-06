e9812043c5793f3398c684b9f6cff3ddb47dc97e
Jillian Gibson <jillian.l.gibson@gmail.com>
2021-01-06 13:38:46 -0800

    adds POST test for sign up

---
+++ __tests__/app.test.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const pool = require('../lib/utils/pool');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('. routes', () => {
+  beforeEach(() => {
+    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
+  });
+
+  it ('allows a user to sign up via POST', () => {
+    //email, password
+    return request(app)
+      .post('api/v1/auth/signup')
+      .send({ email: 'test@test.com', password:'password' })
+      .then(res => {
+        expect(res.body).toEqual({
+          id: expect.any(String),
+          email: 'test@test.com',
+          password: 'password'
+        });
+      });
+  });
+});
