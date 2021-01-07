02c18422e1fc1fc9ceece8c440c82b5dc7d451d6
JustinMartin7x <justin.martin7x@gmail.com>
2021-01-07 14:14:18 -0800

    post route test passing for postgram

--- __tests__/postgram.test.js
+++ __tests__/postgram.test.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
-const Postgram = require('../lib/services/UserService');
+const Postgram = require('../lib/models/Postgram');
 
 
 describe('. routes', () => {
@@ -16,22 +16,26 @@ describe('. routes', () => {
 
 
     it.only ('adds a postgram image url into postgram table using post', async() => {
-        return  request(app)
-        .post('api/v1/postgram')
+      const res = await request(app)
+        .post('/api/v1/postgram')
         .send({
-            userId: 1,
-            photoURL: 'selfphoto.jpg',
-            caption: "cool story bro",
-            tags: ['yolo', 'carpe diem']
-        })
-        .then(res => { 
-            expect(res.body).toEqual({
-            userId: 1,
-            photoURL: 'selfphoto.jpg',
-            caption: "cool story bro",
-            tags: ['yolo', 'carpe diem']
-            }); 
-        });
+          userId: "1",
+          photoURL: 'selfphoto.jpg',
+          caption: "cool story bro",
+          tags: ['yolo', 'carpe diem']
+      })
+
+        expect(res.body).toEqual({
+          id: "1",
+          userId: 1,
+          photoURL: 'selfphoto.jpg',
+          caption: "cool story bro",
+          tags: ['yolo', 'carpe diem']
+      }); 
     });
 
+
+
 });
+
+
